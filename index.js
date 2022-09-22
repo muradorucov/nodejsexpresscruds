0e9ba1d1fdefe8c19452a508e7348ba2c1168fe2
muradorucov <orucowmurad@gmail.com>
2022-09-22 11:01:29 +0400

    post method added

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+let bodyParser = require("body-parser");
+const { parse } = require('path');
 const app = express();
+
+
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+
 
 app.listen(3000, function () {
     console.log('listening on 3000')
@@ -11,17 +19,17 @@ const posts = [
         id: 2,
         name: "Product1",
         unitPrice: 2374
-    }, 
+    },
     {
         id: 2,
         name: "Product1",
         unitPrice: 2374
-    }, 
+    },
     {
         id: 2,
         name: "Product1",
         unitPrice: 2374
-    }, 
+    },
     {
         id: 2,
         name: "Product1",
@@ -31,4 +39,11 @@ const posts = [
 
 app.get('/', (req, res) => {
     res.send(posts)
+})
+
+app.post('/add', (req, res) => {
+    const email = req.body;
+    posts.push(email);
+    console.log(email);
+    res.json(email);
 })
